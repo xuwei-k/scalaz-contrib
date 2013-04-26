298b8c4aa814147f256f39ee6c53c11febdf9728
xuwei-k <6b656e6a69@gmail.com>
2013-04-27 01:33:39 +0900

    wip

--- scala210/main/scala/TryT.scala
+++ scala210/main/scala/TryT.scala
@@ -4,45 +4,47 @@ package std
 import scala.util.control.NonFatal
 import scala.util.{Try, Success, Failure}
 import scalaz._
+import utilTry._
+import scalaz.syntax.foldable._
 
-final case class TryT[F[+_], +T](run: F[Try[T]]){ self =>
+final case class TryT[F[+_], +A](run: F[Try[A]]){ self =>
 
   def isFailure(implicit F: Functor[F]): F[Boolean] = mapT(_.isFailure)
 
   def isSuccess(implicit F: Functor[F]): F[Boolean] = mapT(_.isSuccess)
 
-  def getOrElse[U >: T](default: => U)(implicit F: Functor[F]): F[U] = mapT(_.getOrElse(default))
+  def getOrElse[AA >: A](default: => AA)(implicit F: Functor[F]): F[AA] = mapT(_.getOrElse(default))
 
-  def orElse[U >: T](a: => TryT[F, U])(implicit F: Monad[F]): TryT[F, U] = TryT(
+  def orElse[AA >: A](a: => TryT[F, AA])(implicit F: Monad[F]): TryT[F, AA] = TryT(
     F.bind(run) {
       case Failure(_) => a.run
       case x @ Success(_) => F.point(x)
     }
   )
 
-  def get(implicit F: Functor[F]): F[T] = mapT(_.get)
+  def get(implicit F: Functor[F]): F[A] = mapT(_.get)
 
-  def foreach[U](f: T => U)(implicit F: Each[F]): Unit = F.each(run)(_ foreach f)
+  def foreach[B](f: A => B)(implicit F: Each[F]): Unit = F.each(run)(_ foreach f)
 
-  def flatMap[U](f: T => TryT[F, U])(implicit F: Monad[F]): TryT[F, U] = TryT[F, U](
+  def flatMap[B](f: A => TryT[F, B])(implicit F: Monad[F]): TryT[F, B] = TryT[F, B](
     F.bind(self.run) {
-      case e @ Failure(_) => F.point(e.asInstanceOf[Try[U]])
+      case e @ Failure(_) => F.point(e.asInstanceOf[Try[B]])
       case Success(z) => f(z).run
     }
   )
 
-  def flatMapF[U](f: T => F[U])(implicit F: Monad[F]): TryT[F, U] = TryT[F, U](
+  def flatMapF[B](f: A => F[B])(implicit F: Monad[F]): TryT[F, B] = TryT[F, B](
     F.bind(self.run) {
-      case e @ Failure(_) => F.point(e.asInstanceOf[Try[U]])
+      case e @ Failure(_) => F.point(e.asInstanceOf[Try[B]])
       case Success(z) => F.map(f(z))(Success(_))
     }
   )
 
-  def map[U](f: T => U)(implicit F: Functor[F]): TryT[F, U] = TryT(mapT(_ map f))
+  def map[B](f: A => B)(implicit F: Functor[F]): TryT[F, B] = TryT(mapT(_ map f))
 
-  def filter(p: T => Boolean)(implicit F: Functor[F]): TryT[F, T] = TryT(mapT(_.filter(p)))
+  def filter(p: A => Boolean)(implicit F: Functor[F]): TryT[F, A] = TryT(mapT(_.filter(p)))
 
-  def recoverWith[U >: T](f: PartialFunction[Throwable, TryT[F, U]])(implicit F: Monad[F]): TryT[F, U] = TryT(
+  def recoverWith[AA >: A](f: PartialFunction[Throwable, TryT[F, AA]])(implicit F: Monad[F]): TryT[F, AA] = TryT(
     F.bind(self.run) {
       case x @ Failure(e) =>
         try {
@@ -54,21 +56,55 @@ final case class TryT[F[+_], +T](run: F[Try[T]]){ self =>
     }
   )
 
-  def recover[U >: T](f: PartialFunction[Throwable, U])(implicit F: Functor[F]): TryT[F, U] =
+  def recover[AA >: A](f: PartialFunction[Throwable, AA])(implicit F: Functor[F]): TryT[F, AA] =
     TryT(mapT(_.recover(f)))
 
-  def toOption(implicit F: Functor[F]): OptionT[F, T] =
+  def toOption(implicit F: Functor[F]): OptionT[F, A] =
     OptionT.optionT(F.map(run)(_.toOption))
 
   def failed(implicit F: Functor[F]): TryT[F, Throwable] = TryT(mapT(_.failed))
 
-  def transform[U](s: T => TryT[F, U], f: Throwable => TryT[F, U])(implicit F: Monad[F]): TryT[F, U] = TryT[F, U](
+  def transform[B](s: A => TryT[F, B], f: Throwable => TryT[F, B])(implicit F: Monad[F]): TryT[F, B] = TryT[F, B](
     F.bind(self.run) {
       case Failure(e) => f(e).run
       case Success(z) => s(z).run
     }
   )
 
-  private[this] def mapT[B](f: Try[T] => B)(implicit F: Functor[F]): F[B] = F.map(run)(f)
+  def traverse[G[+_], B](f: A => G[B])(implicit F: Traverse[F], G: Applicative[G]): G[TryT[F, B]] =
+    G.map(F.traverse(run)(o => Traverse[Try].traverse(o)(f)))(TryT(_))
+
+  def foldRight[Z](z: => Z)(f: (A, => Z) => Z)(implicit F: Foldable[F]): Z =
+    F.foldRight[Try[A], Z](run, z)((a, b) => a.foldRight(b)(f))
+
+  private[this] def mapT[B](f: Try[A] => B)(implicit F: Functor[F]): F[B] = F.map(run)(f)
+}
+
+object TryT extends TryTInstances
+
+trait TryTInstances{
+
+}
+
+private[scalaz] trait TryTFunctor[F[+_]] extends Functor[({type λ[α] = TryT[F, α]})#λ] {
+  implicit def F: Functor[F]
+
+  override def map[A, B](fa: TryT[F, A])(f: A => B): TryT[F, B] = fa map f
+}
+
+private[scalaz] trait TryTMonad[F[+_]] extends Monad[({type λ[α] = TryT[F, α]})#λ] with TryTFunctor[F]{
+  implicit def F: Monad[F]
+
+  override def point[A](a: => A): TryT[F, A] = TryT(F.point(Success(a)))
+
+  override def bind[A, B](fa: TryT[F, A])(f: A => TryT[F, B]): TryT[F, B] = fa flatMap f
+}
+
+private[scalaz] trait TryTTraverse[F[+_]] extends Traverse[({type λ[α] = TryT[F, α]})#λ] with TryTFunctor[F]{
+  implicit def F: Traverse[F]
+
+  override def foldRight[A, B](fa: TryT[F, A], z: => B)(f: (A, => B) => B): B = fa.foldRight(z)(f)
+
+  override def traverseImpl[G[+_]: Applicative, A, B](fa: TryT[F, A])(f: A => G[B]): G[TryT[F, B]] = fa traverse f
 }
 
